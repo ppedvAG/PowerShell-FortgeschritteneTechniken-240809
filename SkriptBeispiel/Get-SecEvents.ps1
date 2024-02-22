67661f74aea49eb09390d509e61b7f555f139753
Stefan Ober <StefanO@ppedv.de>
2024-02-22 11:49:16 +0100

    Skript mit PowerShell Default Funktionen/Parametern

--- SkriptBeispiel/Get-SecEvents.ps1
+++ SkriptBeispiel/Get-SecEvents.ps1
@@ -1,3 +1,4 @@
+[cmdletBinding()]
 param(
 [string]$Computername = "localhost",
 
@@ -7,6 +8,8 @@ param(
 [int]$Newest = 5
 )
 
+Write-Verbose -Message "Zusätzliche Ausgabe"
+Write-Verbose -Message "Der User hat folgende Werte angegeben EventId: $EventId"
 
 Get-EventLog -LogName Security -ComputerName $Computername | Where-Object -FilterScript {$PSItem.EventID -eq $EventId} | Select-Object -First $Newest
 
